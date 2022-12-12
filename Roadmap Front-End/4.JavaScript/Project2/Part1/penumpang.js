7c6e67a05276d9fdedc463579dbe518fb8ea790c
Zhephyr <ilhamsyawal87@gmail.com>
2022-12-13 02:28:58 +0700

    Menyelesaikan project penumpang angkot

--- Roadmap Front-End/4.JavaScript/Project2/Part1/penumpang.js
+++ Roadmap Front-End/4.JavaScript/Project2/Part1/penumpang.js
@@ -1,9 +1,21 @@
-var penumpang = [];
+var penumpang = ["ilham", undefined, "jihan"];
+var i;
 var tambahPenumpang = function (namaPenumpang, penumpang) {
-  if ((i=0)) {
+  if (penumpang.length == 0) {
     penumpang.unshift(namaPenumpang);
-    return namaPenumpang;
-  } 
+    return penumpang;
+  } else {
+    for (i = 0; i < penumpang.length; i++) {
+      if (penumpang[i] == undefined) {
+        penumpang[i] = namaPenumpang;
+        return penumpang;
+      } else if (penumpang[i] == namaPenumpang) {
+        console.log("Nama yang dimasukan sudah ada");
+        return penumpang;
+      } else if (i == penumpang.length - 1) {
+        penumpang.push(namaPenumpang);
+        return penumpang;
+      }
+    }
+  }
 };
-
-console.log(tambahPenumpang('ilham', penumpang));
