76229dd92f8a4d8a2fbdf2573ad989a862233d92
Zhephyr <ilhamsyawal87@gmail.com>
2022-12-13 01:16:52 +0700

    Mengerjakan project terbaru

---
+++ Roadmap Front-End/4.JavaScript/Project2/Part1/penumpang.js
@@ -0,0 +1,9 @@
+var penumpang = [];
+var tambahPenumpang = function (namaPenumpang, penumpang) {
+  if ((i=0)) {
+    penumpang.unshift(namaPenumpang);
+    return namaPenumpang;
+  } 
+};
+
+console.log(tambahPenumpang('ilham', penumpang));
