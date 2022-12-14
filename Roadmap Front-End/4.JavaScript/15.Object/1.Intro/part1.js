456f394bb55be5c6a1724800d93a0e48f570f8b4
Zhephyr <ilhamsyawal87@gmail.com>
2022-12-15 04:43:37 +0700

    Mempelajari object

---
+++ Roadmap Front-End/4.JavaScript/15.Object/1.Intro/part1.js
@@ -0,0 +1,12 @@
+// !Membuat object
+
+var mhs = {
+  nama: "Ilham",
+  umur: 23,
+  ipk: [3.0, 2.5, 3.2],
+  alamat: {
+    Jalan: "Angsana 3",
+    Kota: "Bogor",
+    Provinsi: "Jawa Barat",
+  },
+};
