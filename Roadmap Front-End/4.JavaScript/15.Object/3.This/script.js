3f0079a04e9ecd6623fa804da41a30466d32149e
Zhephyr <ilhamsyawal87@gmail.com>
2022-12-16 22:38:14 +0700

    Mempelajari This

---
+++ Roadmap Front-End/4.JavaScript/15.Object/3.This/script.js
@@ -0,0 +1,21 @@
+// !Function declaration
+// function halo() {
+//     console.log('Halo');
+// }
+
+// ! Object Literal
+
+// var obj = {};
+// obj.halo = function() {
+//     console.log('Halo');
+// }
+
+// obj.halo();
+
+
+// ! Contruction
+// function Halo () {
+//     console.log('Halo');
+// }
+
+// new Halo();
