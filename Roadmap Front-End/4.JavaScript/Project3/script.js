3a66047455cd549b50b2107da5f372bd6ca93f00
Zhephyr <ilhamsyawal87@gmail.com>
2022-12-22 07:09:12 +0700

    Bikin alur dulu

---
+++ Roadmap Front-End/4.JavaScript/Project3/script.js
@@ -0,0 +1,9 @@
+//! Bikin function angkot yg berisi nama, jurusan angkot, array penumpang, kas;
+
+    //* di dalemnya buat function untuk menambah penumpang dengan parameter nama
+
+    //* Buat function untuk mengurangi penumpang dengan parameter nama serta kas.
+
+
+
+//? Bikin beberapa angkot
