09a08793b3e98fe2a67de1f75dc382e4bfdbacd8
Zhephyr <ilhamsyawal87@gmail.com>
2023-01-18 09:49:50 +0700

    Mempelajari DOM Manipulation

---
+++ Roadmap Front-End/4.JavaScript Lanjutan/2.DOMManipulation/Part1/script.js
@@ -0,0 +1,43 @@
+// Element.innerHTML
+var judul = document.getElementById("judul");
+judul.innerHTML = " <em> Yanqeu cantik </em> ";
+
+// Element.style
+const sectionA = document.querySelector("section#a");
+sectionA.style.backgroundColor = "lightblue";
+sectionA.style.color = "salmon";
+
+// element.setAttribute()
+const judul = document.getElementsByTagName("h1")[0];
+judul.setAttribute("class", "Ilham");
+
+const a = document.querySelector("#a a");
+a.setAttribute("id", "link");
+
+// Element.classList 
+const p2 = document.querySelector(".p2");
+
+    // classList.add
+p2.classList.add("kanjut");
+
+    // classList.remove
+p2.classList.remove("kanjut"); 
+
+    // classList.toggle
+p2.classList.toggle('kanjut');
+
+document.body.classList.toggle('biru-muda');
+
+    // classList.item
+p2.classList.add('satu');
+p2.classList.add('dua');
+p2.classList.add('tiga');
+
+
+p2.classList.item(2);
+
+    // classList.contains
+p2.classList.contains('empat');
+
+    // classList.replace
+p2.classList.replace('dua', 'lima');
