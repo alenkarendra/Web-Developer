75ccba39490fe2d1ce7cec3498d5d6e9677d2e57
Zhephyr <ilhamsyawal87@gmail.com>
2023-01-19 23:54:04 +0700

    Mempelajari DOM Manipulation Part2

--- Roadmap Front-End/4.JavaScript Lanjutan/2.DOMManipulation/Part1/script.js
+++ Roadmap Front-End/4.JavaScript Lanjutan/2.DOMManipulation/Part1/script.js
@@ -1,3 +1,5 @@
+// DOM Manipulation Part1
+
 // Element.innerHTML
 var judul = document.getElementById("judul");
 judul.innerHTML = " <em> Yanqeu cantik </em> ";
@@ -14,30 +16,29 @@ judul.setAttribute("class", "Ilham");
 const a = document.querySelector("#a a");
 a.setAttribute("id", "link");
 
-// Element.classList 
+// Element.classList
 const p2 = document.querySelector(".p2");
 
-    // classList.add
+// classList.add
 p2.classList.add("kanjut");
 
-    // classList.remove
-p2.classList.remove("kanjut"); 
+// classList.remove
+p2.classList.remove("kanjut");
 
-    // classList.toggle
-p2.classList.toggle('kanjut');
+// classList.toggle
+p2.classList.toggle("kanjut");
 
-document.body.classList.toggle('biru-muda');
+document.body.classList.toggle("biru-muda");
 
-    // classList.item
-p2.classList.add('satu');
-p2.classList.add('dua');
-p2.classList.add('tiga');
-
+// classList.item
+p2.classList.add("satu");
+p2.classList.add("dua");
+p2.classList.add("tiga");
 
 p2.classList.item(2);
 
-    // classList.contains
-p2.classList.contains('empat');
+// classList.contains
+p2.classList.contains("empat");
 
-    // classList.replace
-p2.classList.replace('dua', 'lima');
+// classList.replace
+p2.classList.replace("dua", "lima");
--- Roadmap Front-End/4.JavaScript Lanjutan/2.DOMManipulation/Part2/script.js
+++ Roadmap Front-End/4.JavaScript Lanjutan/2.DOMManipulation/Part2/script.js
@@ -1,2 +1,51 @@
-// DOM Manipulation
+// DOM Manipulation Part2
 
+//! Percobaan 1
+// Buat element dan isinya
+const pBaru = document.createElement("p");
+const teksPBaru = document.createTextNode("Paragraf baru");
+
+// Penggabungan
+pBaru.appendChild(teksPBaru);
+
+// Tangkep parent
+const sectionA = document.getElementById("a");
+
+// Show di akhir section a
+sectionA.appendChild(pBaru);
+
+//! Percobaan 2
+// Buat Element
+const liBaru = document.createElement("li");
+const isinya = document.createTextNode("Item Baru");
+
+// Penggabungan
+liBaru.appendChild(isinya);
+
+// Tangkep parent 
+const sectionB = document.querySelector("section#b ul");
+const li2 = sectionB.querySelector("li:nth-child(2)");
+
+// Show di HTML
+sectionB.insertBefore(liBaru, li2);
+
+//! Percobaan 3
+const link = document.getElementsByTagName("a")[0];
+
+sectionA.removeChild(link);
+
+//! Percobaan 4
+const bagian2 = document.getElementById("b");
+const p4 = bagian2.getElementsByTagName("p")[0];
+
+const h2Baru = document.createElement("h2");
+const isiH2 = document.createTextNode("Halo semuanya");
+
+h2Baru.appendChild(isiH2);
+
+bagian2.replaceChild(h2Baru, p4);
+
+// Informasi
+pBaru.style.backgroundColor = "lightgreen";
+liBaru.style.backgroundColor = "lightgreen";
+h2Baru.style.backgroundColor = "lightgreen";
