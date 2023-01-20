ba8211b3a1c8a91789023e31e94ec36427c3eb46
Zhephyr <ilhamsyawal87@gmail.com>
2023-01-20 15:39:32 +0700

    Belajar DOM Events

---
+++ Roadmap Front-End/4.JavaScript Lanjutan/3.DOMEvents/script.js
@@ -0,0 +1,27 @@
+// Percobaan 1
+
+const p3 = document.querySelector(".p3");
+
+function ubahWarnaP3() {
+  p3.style.backgroundColor = "lightblue";
+}
+
+// Percobaan 2
+
+const p2 = document.querySelector(".p2");
+
+function ubahWarnaP2() {
+  p2.style.backgroundColor = "lightblue";
+}
+
+p2.onclick = ubahWarnaP2;
+
+// Percobaan 3
+const p4 = document.querySelector("section#b p");
+p4.addEventListener("click", function () {
+  const ul = document.querySelector("section#b ul");
+  const li = document.createElement("li");
+  const isi = document.createTextNode("Item baru");
+  li.appendChild(isi);
+  ul.appendChild(li);
+});
