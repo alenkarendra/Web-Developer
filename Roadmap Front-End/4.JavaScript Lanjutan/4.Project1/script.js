b3e61a5a9d61b183e2c486cedde50e4bffb5c3ae
Zhephyr <ilhamsyawal87@gmail.com>
2023-01-26 16:40:53 +0700

    Merubah script.js dikit

--- Roadmap Front-End/4.JavaScript Lanjutan/4.Project1/script.js
+++ Roadmap Front-End/4.JavaScript Lanjutan/4.Project1/script.js
@@ -19,6 +19,7 @@ tBaru.addEventListener("click", function () {
   const r = Math.round(Math.random() * 255 + 1);
   const g = Math.round(Math.random() * 255 + 1);
   const b = Math.round(Math.random() * 255 + 1);
+  
   document.body.style.backgroundColor = "rgb(" + r + ", " + g + ", " + b + ") ";
 });
 
