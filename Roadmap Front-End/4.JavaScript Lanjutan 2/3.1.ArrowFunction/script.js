1db4cd74dfaba0ccc7bd6714b88e8ee626cbd34f
Zhephyr <ilhamsyawal87@gmail.com>
2023-05-11 16:16:22 +0700

    Mempelajari mengenai arrow function

---
+++ Roadmap Front-End/4.JavaScript Lanjutan 2/3.1.ArrowFunction/script.js
@@ -0,0 +1,49 @@
+//! Function Declaration !//
+function cobaan(siapa) {
+  alert(`Halo, ${siapa}`);
+}
+
+cobaan("kuda");
+
+//! Function Expression !//
+let hasil = function (nama) {
+  return `Halo, ${nama}`;
+};
+
+console.log(hasil("ilham"));
+
+//! Arrow Function !//
+// Tanpa argument
+let enol = () => `Hello World !`;
+
+// Satu argument
+let satu = (kita) => `Halo, ${kita}`;
+
+// Banyak argument
+let baru = (parameter, waktu) => {
+  return `Hai, ${parameter}. Sekarang jam ${waktu}`;
+};
+
+console.log(enol());
+console.log(satu("shanum"));
+console.log(baru("jihan", "sepuluh"));
+
+// Contoh lain
+let mahasiswa = ["Ilham Syawal M", "Jihan Febrizki", "Shanum"];
+
+// Cara lama
+// let totalHuruf = mahasiswa.map(function (nama) {
+//   return nama.length;
+// });
+// console.log(totalHuruf);
+
+// Dengan Arrow Function
+let totalHuruf = mahasiswa.map((nama) => nama.length);
+
+console.log(totalHuruf);
+
+// return object
+
+let informasi = mahasiswa.map((nama) => ({ nama, jumlahHuruf: nama.length }));
+
+console.table(informasi);
