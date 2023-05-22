faad844436d23d623e26f4ca23f2dea97dde63ed
Zhephyr <ilhamsyawal87@gmail.com>
2023-05-23 00:45:37 +0700

    menambahkan selain javascript

--- Roadmap Front-End/4.JavaScript Lanjutan 2/4.3.LatihanFIlterMapReduce/script.js
+++ Roadmap Front-End/4.JavaScript Lanjutan 2/4.3.LatihanFIlterMapReduce/script.js
@@ -33,7 +33,31 @@ pJumlah.innerHTML = jmlVideo + " video.";
 const pDurasi = document.querySelector(".total-durasi");
 pDurasi.innerHTML = `${jam} jam, ${menit} menit, ${detik} detik.`;
 
-// Bikin untuk memperlihatkan seluruh nilai dari yg bukan js lanjutan
-const bukanJs = videos.length - jmlVideo;
 
-console.log(bukanJs);
+// Untuk selain Javascript lanjutan
+//! Total waktunya
+let bukanJs = videos
+  .filter((video) => !video.innerHTML.includes("JAVASCRIPT LANJUTAN"))
+  .map((item) => item.dataset.duration)
+  .map((waktu) => {
+    const parts = waktu.split(":").map((part) => parseFloat(part));
+    return parts[0] * 60 + parts[1];
+  })
+  .reduce((awal, akhir) => awal + akhir, 0);
+
+//! Dipisah antara jam, menit, detik
+const jam1 = Math.floor(bukanJs / 3600);
+const lanjutan = bukanJs - jam1 * 3600;
+const menit1 = Math.floor(lanjutan / 60);
+const detik1 = lanjutan - menit1 * 60;
+
+//! Di munculkan dengan DOM
+const jumlahVideo = document.querySelector(".jumlahan-video");
+const totalDurasi = document.querySelector(".totalan-durasi");
+
+const lainnya = videos.filter(
+  (video) => !video.innerHTML.includes("JAVASCRIPT LANJUTAN")
+).length;
+jumlahVideo.innerHTML = lainnya + " video.";
+
+totalDurasi.innerHTML = `${jam1} jam, ${menit1} menit, ${detik1} detik.`
