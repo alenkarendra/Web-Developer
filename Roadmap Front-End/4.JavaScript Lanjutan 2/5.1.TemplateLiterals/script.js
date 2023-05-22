5a6508dfad8b05296a08213c95b992b06c1c0ccb
Zhephyr <ilhamsyawal87@gmail.com>
2023-05-22 15:37:15 +0700

    mempelajari template literals

---
+++ Roadmap Front-End/4.JavaScript Lanjutan 2/5.1.TemplateLiterals/script.js
@@ -0,0 +1,14 @@
+console.log(`Halo semuanya`);
+
+console.log(`tes 1
+tes 2
+tes 3`);
+
+const nama = "Ilham Syawal Mustaqim";
+const umur = 24;
+
+console.log(`Halo, nama saya ${nama}, saya berumur ${umur} tahun`);
+
+const x = 191;
+
+console.log(`${x % 2 == 0 ? "genap" : "ganjil"}`);
