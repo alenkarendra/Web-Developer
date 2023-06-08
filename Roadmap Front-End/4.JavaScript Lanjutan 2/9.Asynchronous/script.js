170c9bbfbe795ff69dc4955129457f005eda4024
Zhephyr <ilhamsyawal87@gmail.com>
2023-06-08 14:49:50 +0700

    mempelajari mengenai asynchronous

---
+++ Roadmap Front-End/4.JavaScript Lanjutan 2/9.Asynchronous/script.js
@@ -0,0 +1,10 @@
+// Contoh asynchronous callback
+
+console.log("satu");
+setTimeout(() => {
+  console.log("dua");
+}, 10000);
+console.log("tiga");
+console.log("empat");
+console.log("lima");
+console.log("enam");
